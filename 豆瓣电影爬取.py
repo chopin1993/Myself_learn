faf395bb25edb5a608e24ca449d000dce10e3856
chopin1993 <15122827500@163.com>
2021-01-24 20:12:50 +0800

    Default Changelist

--- 豆瓣电影爬取.py
+++ 豆瓣电影爬取.py
@@ -1,13 +1,15 @@
 # coding:utf-8
-import requests
 import json
+import time
+import requests
 
 url = "https://movie.douban.com/j/search_subjects"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
 }
+type = input("输入：")
 param = {
-    "type": "movie",
+    "type": type,
     "tag": "热门",
     "sort": "recommend",
     "page_limit": "20",
@@ -16,5 +18,5 @@ param = {
 response = requests.get(url=url, params=param, headers=headers)
 list_file = response.json()
 fp = open("./douban.json", "w", encoding="utf-8")
-json.dump(list_file,fp=fp,ensure_ascii=False)   # 将Python数据结构转换成json数据，json.loads将json转换成Python数据结构
-print("爬取完成！")
+json.dump(list_file, fp=fp, ensure_ascii=False)  # 将Python数据结构转换成json数据，json.loads将json转换成Python数据结构
+print("豆瓣电影爬取完成！！---->时间戳：", time.asctime(time.localtime(time.time())))
